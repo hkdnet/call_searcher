cead933c4d917272379cd5a9d3d06ce2e88ad05d
hkdnet <satoko.itse@gmail.com>
2018-08-26 21:20:00 +0900

    Remove an unnnecessary spec

--- spec/call_searcher_spec.rb
+++ spec/call_searcher_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe CallSearcher do
   it "has a version number" do
     expect(CallSearcher::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
